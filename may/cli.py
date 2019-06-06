2cfa6f26fc0cb3115217e6221102f7bf6a354764
Sam Nolan <samnolan555@gmail.com>
2019-06-06 20:02:00 +1000

    Add better options support

--- may/cli.py
+++ may/cli.py
@@ -5,51 +5,73 @@ Email: sam.nolan@rmit.edu.au
 Github: https://github.com/Hazelfire
 Description: A click cli for may
 """
-from functools import wraps
 from importlib.resources import read_text
 from requests.exceptions import ConnectionError
 import requests
 
 from quickconfig import Config
-from graphjinj import run
 import docsep
 from jinja2 import Template
 from docopt import docopt
 import sys
 import yaml
+import json
 
 conf = Config("may")
 BASE_URL = "http://localhost:8000/graphql"
 
+def get_view_file(arguments):
+    if len(arguments) > 1 and not arguments[1].startswith("-"):
+        view_file = read_text("may.views." + arguments[0], arguments[1] + ".gjinj")
+    else:
+        view_file = read_text("may.views", arguments[0] + ".gjinj")
+    return view_file
+
+
 def run_view(docs, arguments):
     """ Runs a may view """
+
+    # Getting the view
     view_file = None
     try:
-        if len(arguments) > 1 and not arguments[1].startswith("--"):
-            view_file = read_text("may.views." + arguments[0], arguments[1] + ".gjinj")
-        else:
-            view_file = read_text("may.views", arguments[0] + ".gjinj")
+        view_file = get_view_file(arguments)
     except FileNotFoundError:
         print(docs.strip())
         return
+
     documents = docsep.parse(view_file)
+
+    # Parse the help doc string
     args = docopt(documents["command"].body, argv=arguments, help=True)
 
+
+    # Create graphql variables from that
     variables = {}
     if "variables" in documents:
         variables = yaml.load(Template(documents["variables"].body).render(args))
 
 
+    # Add Auth token
     session = requests.Session()
     if "token" in conf:
         session.headers.update({"Authorization": "JWT " + conf["token"]})
-    res = run(
+
+    
+    # Make request
+    response= session.post(
         BASE_URL,
-        view_file,
-        variables,
-        session=session,
-    )
-    print(res.display)
+        data={
+            "query": documents["query"].body,
+            "variables": json.dumps(variables),
+        },
+    ).json()
+
+    # View request
+    display = Template(documents["display"].body.strip()).render({
+        **response,
+        'args': dict(args)
+    }).strip()
+    print(display)
 
 doc = """
 Usage: may [--version] [--help] [<command>] [<args>...]
