0b98a4d1b6d13e976971ef1251387a61721efe7d
Sam Nolan <samnolan555@gmail.com>
2019-06-06 19:24:20 +1000

    Made the may CLI almost fully declarative

--- may/cli.py
+++ may/cli.py
@@ -7,107 +7,68 @@ Description: A click cli for may
 """
 from functools import wraps
 from importlib.resources import read_text
-import click
 from requests.exceptions import ConnectionError
 import requests
 
-from .api import MayApi, GraphQLException
 from quickconfig import Config
 from graphjinj import run
+import docsep
+from jinja2 import Template
+from docopt import docopt
+import sys
+import yaml
 
 conf = Config("may")
 BASE_URL = "http://localhost:8000/graphql"
 
-def fails_gracefully(command):
-    """ Wraps a command so that it fails gracefully when exceptions are thrown """
-    @wraps(command)
-    def wrapper(*args, **kwargs):
-        try:
-            command(*args, **kwargs)
-        except GraphQLException as e:
-            click.echo("GraphQL Error: " + str(e), err=True)
-        except ConnectionError as e:
-            click.echo("Could not connect to url")
-    return wrapper
-
-def requires_auth(command):
-    """ Wrapper for commands that require auth before executing """
-    @wraps(command)
-    @fails_gracefully
-    def wrapper(*args, **kwargs):
-        if "token" in conf:
-            click.echo("Operation requires logging in, try may login", err=True)
-            return
-        command(*args, **kwargs)
-    return wrapper
-
-def run_view(template, variables=None):
+def run_view(template, arguments, package=None):
     """ Runs a may view """
+    view_file = None
+    if package:
+        view_file = read_text("may.views." + package, template + ".gjinj")
+    else:
+        view_file = read_text("may.views", template + ".gjinj")
+    documents = docsep.parse(view_file)
+    args = docopt(documents["command"].body, argv=arguments, help=True)
+
+    variables = {}
+    if "variables" in documents:
+        variables = yaml.load(Template(documents["variables"].body).render(args))
+
+
     session = requests.Session()
-    if not variables:
-        variables = {}
     if "token" in conf:
         session.headers.update({"Authorization": "JWT " + conf["token"]})
     res = run(
         BASE_URL,
-        read_text("may.views", template),
+        view_file,
         variables,
         session=session,
     )
-    return res
+    print(res.display)
 
+doc = """
+Usage: may [--version] [--help] [<command>] [<subcommand>] [<args>...]
 
-@click.group()
+Commands include:
+    task
+    folder
+    login
+"""
+
 def cli():
-    """ Click main cli group """
+    """ Main entry point """
+    args = docopt(doc, version="May version 0.0.1", options_first=True, help=True)
+    if args["<command>"]:
+        if args["<subcommand>"]:
+            run_view(args["<subcommand>"], package=args["<command>"], arguments=[args["<command>"], args["<subcommand>"]] + args["<args>"])
+        else:
+            run_view(args["<command>"], [args["<command>"]] + args["<args>"])
+    else:
+        print(doc)
+            
 
 
-@cli.command()
-@fails_gracefully
-def login():
-    """ Logs the token to file """
-    username = click.prompt("Username")
-    password = click.prompt("Password")
-
-    result = run_view(
-        "login.gjinj",
-        {'username': username, 'password': password}
-    )
-    if "data" in result.json:
-        conf["token"] = result.json["data"]["tokenAuth"]["token"]
-    click.echo(result.display)
-
-@cli.group()
-def task():
-    """ task related commands """
-
-@task.command(name="add")
-@click.argument("name")
-@click.argument("duration", type=int)
-@fails_gracefully
-def task_add(name, duration):
-    """ adds a task """
-    click.echo(run_view("addTask.gjinj", {
-        'input': {
-            'name': name,
-            'duration': duration
-        }
-    }).display)
-
-@task.command(name="complete")
-@click.argument("taskid")
-@fails_gracefully
-def task_complete(taskid):
-    """ complete a task """
-    click.echo(run_view("completeTask.gjinj", {
-        'input': {
-            'id': taskid
-        }
-    }).display)
 
 
-@task.command(name="list")
-@fails_gracefully
-def task_list():
-    """ returns a list of tasks """
-    click.echo(run_view("getTasks.gjinj").display)
+
